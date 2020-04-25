956daa31ee8146d5cb4f42352558335eff3b58f6
DiegoFR <diegofreire8482@hotmail.com>
2020-04-24 22:26:52 -0300

    Fixed some things in Javascript and updated CSS

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@ const input = document.querySelector("input");
 const content = document.getElementById("content");
 const arrow = document.getElementById("arrow");
 const button = document.querySelector("button");
-const email = document.getElementById("email");
 
 function validateEmail(email){
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;    
@@ -15,35 +14,24 @@ function validateEmail(email){
 function generateError(){
     console.log(`Email Inválido: ${email.value}`);
     input.style.borderColor = "red";
-    errorParagraph = document.createElement("p");
-    errorParagraph.style.marginTop = "10px";
-    errorParagraph.style.marginLeft = "16px";
-    content.appendChild(errorParagraph);
+    errorDiv = document.createElement("div");
+    errorDiv.setAttribute("id", "error");
+    content.appendChild(errorDiv);
     errorMessage = document.createTextNode("Please provide a valid email");
-    errorParagraph.appendChild(errorMessage);
-    errorParagraph.style.color = "red";
+    errorDiv.appendChild(errorMessage);
 
-    errorIcon = document.createElement("p");
-    errorIconText = document.createTextNode("imagem de erro");
-    errorIcon.appendChild(errorIconText);
-    errorIcon.style.height = "24px";
-    errorIcon.style.lineHeight = "24px";
-    errorIcon.style.lineWidth = "24px";
-    errorIcon.style.width = "24px";
-    errorIcon.style.margin = "0";
-    errorIcon.style.transform = "translate(-40px, -12px)";
-    errorIcon.style.textIndent = "-9999px";
-    errorIcon.style.position = "relative";
-    errorIcon.style.background = "url(images/icon-error.svg) no-repeat right";
-    button.appendChild(errorIcon);
-
-    arrow.style.transform = "translateY(11px)";
+    errorIcon = document.createElement("img");
+    errorIcon.setAttribute("src", "images/icon-error.svg");
+    errorDiv.appendChild(errorIcon);
 }
 
 function submitEmail(email){
-    if(validateEmail(email)==false){
-        generateError();
-    }else    
-        console.log(`Email Válido: ${email.value}`);
-        
+    if(email.value!=""){
+        if(validateEmail(email)==false){
+            generateError();
+            event.preventDefault();
+        }else
+            console.log(`Email Válido: ${email.value}`);
+    }else
+        console.log(`Campo não preenchido.`);
 }
