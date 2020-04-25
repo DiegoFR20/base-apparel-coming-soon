9bbc8d7bd49b2cc51103734f55ba4d8374e9ffa2
DiegoFR20 <diegofreire8482@hotmail.com>
2020-04-25 14:18:14 -0300

    Finished

--- scripts.js
+++ scripts.js
@@ -1,30 +1,36 @@
 const input = document.querySelector("input");
 const content = document.getElementById("content");
-const arrow = document.getElementById("arrow");
-const button = document.querySelector("button");
+var contError = 0;
 
 function validateEmail(email){
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;    
     if(email.value.match(mailformat)){
+        input.style.borderColor = "green";
         return true;
     }else
         return false;
 }
 
 function generateError(){
-    console.log(`Email Inválido: ${email.value}`);
-    input.style.borderColor = "red";
-    errorDiv = document.createElement("div");
-    errorParagraph = document.createElement('p');
-    errorDiv.setAttribute("id", "error");
-    content.appendChild(errorDiv);
-    errorMessage = document.createTextNode("Please provide a valid email");
-    errorParagraph.appendChild(errorMessage);
-    errorDiv.appendChild(errorParagraph);
-
-    errorIcon = document.createElement("img");
-    errorIcon.setAttribute("src", "images/icon-error.svg");
-    errorDiv.appendChild(errorIcon);
+    if(contError==0){
+        input.style.borderColor = "red";
+        errorDiv = document.createElement("div");
+        errorParagraph = document.createElement('p');
+        errorDiv.setAttribute("id", "error");
+        content.appendChild(errorDiv);
+        errorMessage = document.createTextNode("Please provide a valid email");
+        errorParagraph.appendChild(errorMessage);
+        errorDiv.appendChild(errorParagraph);
+        errorIcon = document.createElement("img");
+        errorIcon.setAttribute("src", "images/icon-error.svg");
+        errorDiv.appendChild(errorIcon);
+        setTimeout(() => {
+            input.style.borderColor = "";
+            content.removeChild(errorDiv);
+            contError=0;
+        }, 5000);
+    }
+    contError++;
 }
 
 function submitEmail(email){
@@ -32,8 +38,6 @@ function submitEmail(email){
         if(validateEmail(email)==false){
             generateError();
             event.preventDefault();
-        }else
-            console.log(`Email Válido: ${email.value}`);
-    }else
-        console.log(`Campo não preenchido.`);
+        }
+    }
 }
